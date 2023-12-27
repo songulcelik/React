041b1834372a7d6ab58ba145f8a474f7b5a599dc
Songül Çelik <songulcelik3537@gmail.com>
2023-12-27 22:26:38 +0300

    v-1

--- src/components/19-birthday/bithday.jsx
+++ src/components/19-birthday/bithday.jsx
@@ -7,16 +7,23 @@ const Birthday = () => {
 
     const [people, setPeople] = useState(data);
 
+    const deletePerson = (id) => {
+      if(!window.confirm("Are you sure to delete")) return;
+      const arr=people.filter(item=>item.id !== id);
+      setPeople(arr);
+    }
+    
+
     
 
   return (
     <Container className='my-3 py-3'>
         <h1>Bugun Doganlar</h1>
         <p>Dogun gunu bugun olan {people.length} kisi bulundu</p>
-        {people.map(item=> <Person key={item.id} {...item}/>)}
+        {people.map(item=> <Person key={item.id} {...item} deletePerson={deletePerson}/>)}
 
         <Button variant='danger' onClick={()=>setPeople([])} >Temizle</Button>
-        <Button variant='info' onClick={()=>setPeople(data)}>Yenile</Button>
+        <Button variant='info'className='ms-3' onClick={()=>setPeople(data)}>Yenile</Button>
     </Container>
   )
 }
--- src/components/19-birthday/person.jsx
+++ src/components/19-birthday/person.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card, CardBody, CardSubtitle, CardTitle, Col, Image, Row } from 'react-bootstrap'
-import { LiaBirthdayCakeSolid } from "react-icons/lia";
-const Person = ({id,image,name,age}) => {
+import { LiaBirthdayCakeSolid, LiaTrashAlt } from "react-icons/lia";
+const Person = ({id,image,name,age,deletePerson}) => {
   return (
     <Card className='mb-3'>
        <CardBody>
@@ -15,6 +15,9 @@ const Person = ({id,image,name,age}) => {
             </Col>
         </Row>
        </CardBody>
+       <span className='position-absolute end-0 me-3 mt-2 fs-2 text-danger ' style={{cursor:"pointer"}} onClick={() =>deletePerson(id)}>
+        <LiaTrashAlt/>
+       </span>
     </Card>
   )
 }
